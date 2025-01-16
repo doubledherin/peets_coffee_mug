8c2094261b9f1068d29dace41f7f6bdfcad68e6a
Wendy Dherin <wendydherin@gmail.com>
2025-01-16 15:09:42 -0800

    Done.

--- shape.js
+++ shape.js
@@ -15,13 +15,222 @@ class Shape {
     this.cellSize = cellSize
     this.numRows = 8
     this.dotSize = 4
-    this.padding = 2
+    this.padding = this.dotSize / 2
     this.numElements = random([1, 2, 3, 4])
-    this.type = random(["left", "right", "up", "down", "hourglass", "diamond"])
-    this.fillType = random(["solid", "dotted, three-dotted"])
   }
 
-  triangleOf3Dots(x, y, direction, dotColor) {
+  draw() {
+    stroke(255, 240, 219)
+    strokeWeight(2)
+    switch (this.numElements) {
+      case 1:
+        this.drawOneElement()
+        break
+      case 2:
+        this.drawTwoElements()
+        break
+      case 3:
+        this.drawThreeElements()
+        break
+      case 4:
+        this.drawFourElements()
+        break
+    }
+  }
+
+  drawOneElement() {
+    const element = random(["triangle", "hourglass", "diamond"])
+    const color = random(this.colors)
+    switch (element) {
+      case "triangle":
+        const direction = random(["up", "right", "down", "left"])
+        if (random() < 0.5) {
+          this.filledTriangle(direction, color)
+        } else {
+          this.triangleOfDots(this.x, this.y, direction, color)
+        }
+        break
+      case "hourglass":
+        this.hourglass(color)
+        break
+      case "diamond":
+        this.diamond(this.x, this.y, color)
+        break
+    }
+  }
+
+  drawTwoElements() {
+    const element = random([
+      "diamondWithTriangle",
+      "hourglassWithTriangleToTheLeft",
+      "hourglassWithDiamondOnRight",
+    ])
+    switch (element) {
+      case "diamondWithTriangle":
+        this.diamondWithTriangle()
+        break
+      case "hourglassWithTriangleToTheLeft":
+        this.hourglassWithTriangleToTheLeft()
+        break
+      case "hourglassWithDiamondOnRight":
+        this.hourglassWithDiamondOnRight()
+        break
+    }
+  }
+
+  drawThreeElements() {
+    const element = random([
+      "hourglassWithTwoTrianglesOnLeft",
+      "hourglassWithTwoTriangleOnRight",
+      "hourglassWithTriangleOnLeftAndDiamondOnRight",
+    ])
+    const hourglassColor = random(this.colors)
+    switch (element) {
+      case "hourglassWithTwoTrianglesOnLeft":
+        this.hourglassWithTwoTrianglesOnLeft(hourglassColor)
+        break
+      case "hourglassWithTwoTriangleOnRight":
+        this.hourglassWithTwoTriangleOnRight(hourglassColor)
+        break
+      case "hourglassWithTriangleOnLeftAndDiamondOnRight":
+        this.hourglassWithTriangleOnLeftAndDiamondOnRight(hourglassColor)
+        break
+    }
+  }
+
+  drawFourElements() {
+    const element = random([
+      "hourglassWithTwoTrianglesOnLeftAndDiamondOnRight",
+      "hourglassWithTwoTriangleOnRightAndDiamondOnLeft",
+    ])
+    const hourglassColor = random(this.colors)
+    switch (element) {
+      case "hourglassWithTwoTrianglesOnLeftAndDiamondOnRight":
+        this.hourglassWithTwoTrianglesOnLeftAndDiamondOnRight(hourglassColor)
+        break
+      case "hourglassWithTwoTriangleOnRightAndDiamondOnLeft":
+        this.hourglassWithTwoTriangleOnRightAndDiamondOnLeft(hourglassColor)
+        break
+    }
+  }
+
+  filledTriangle(direction, color) {
+    fill(color)
+    switch (direction) {
+      case "left":
+        triangle(
+          this.x,
+          this.y,
+          this.x + this.cellSize,
+          this.y - this.cellSize,
+          this.x + this.cellSize,
+          this.y + this.cellSize
+        )
+        break
+      case "right":
+        triangle(
+          this.x,
+          this.y,
+          this.x - this.cellSize,
+          this.y - this.cellSize,
+          this.x - this.cellSize,
+          this.y + this.cellSize
+        )
+        break
+      case "down":
+        triangle(
+          this.x,
+          this.y,
+          this.x - this.cellSize,
+          this.y - this.cellSize,
+          this.x + this.cellSize,
+          this.y - this.cellSize
+        )
+        break
+      case "up":
+        triangle(
+          this.x,
+          this.y,
+          this.x - this.cellSize,
+          this.y + this.cellSize,
+          this.x + this.cellSize,
+          this.y + this.cellSize
+        )
+        break
+    }
+  }
+
+  triangleOfDots(x, y, direction, dotColor) {
+    const angleMap = {
+      up: 0,
+      right: HALF_PI,
+      down: PI,
+      left: -HALF_PI,
+    }
+    const angle = angleMap[direction]
+
+    push()
+    noStroke()
+    translate(x, y)
+    rotate(angle)
+    translate(-x, -y)
+
+    fill(dotColor)
+    for (let i = 0; i < this.numRows; i++) {
+      let numDotsInRow = i * 2 + 1
+      const widthOfAllDotsInRow =
+        numDotsInRow * (this.dotSize + this.padding) - this.padding
+      const xCoordinateOfFirstDotInRow =
+        x - widthOfAllDotsInRow / 2 + this.padding
+      for (let j = 0; j < numDotsInRow; j++) {
+        const dotX =
+          xCoordinateOfFirstDotInRow + j * (this.dotSize + this.padding)
+        const dotY = y + i * (this.dotSize + this.padding) + this.padding * 2
+        ellipse(dotX, dotY, this.dotSize)
+      }
+    }
+    pop()
+  }
+
+  diamond(x, y, diamondColor) {
+    push()
+    noStroke()
+    this.triangleOfDots(x, y - this.cellSize, "up", diamondColor)
+    this.triangleOfDots(x, y + this.cellSize, "down", diamondColor)
+    pop()
+  }
+
+  hourglass(c) {
+    fill(c)
+    triangle(
+      this.x,
+      this.y,
+      this.x - this.cellSize,
+      this.y + this.cellSize,
+      this.x + this.cellSize,
+      this.y + this.cellSize
+    )
+    triangle(
+      this.x,
+      this.y,
+      this.x - this.cellSize,
+      this.y - this.cellSize,
+      this.x + this.cellSize,
+      this.y - this.cellSize
+    )
+  }
+
+  hourglassWithDiamondOnRight(hourglassColor) {
+    const diamondColor = random(this.colors.filter((c) => c !== hourglassColor))
+    this.diamond(this.x + this.cellSize, this.y, diamondColor)
+  }
+
+  hourglassWithDiamondToTheLeft(hourglassColor) {
+    const diamondColor = random(this.colors.filter((c) => c !== hourglassColor))
+    this.diamond(this.x - this.cellSize, this.y, diamondColor)
+  }
+
+  triangleOfThreeDots(x, y, direction, dotColor) {
     const numRows = 6
     const angleMap = {
       up: 0,
@@ -32,6 +241,7 @@ class Shape {
     const angle = angleMap[direction]
 
     push()
+    noStroke()
     translate(x, y)
     rotate(angle)
     translate(-x, -y)
@@ -65,91 +275,14 @@ class Shape {
     ellipse(dotX + dotSize * 2, dotY, dotSize)
   }
 
-  triangleOfDots(x, y, direction, dotColor) {
-    const angleMap = {
-      up: 0,
-      right: HALF_PI,
-      down: PI,
-      left: -HALF_PI,
-    }
-    const angle = angleMap[direction]
-
-    push()
-    translate(x, y)
-    rotate(angle)
-    translate(-x, -y)
-    // noStroke()
-    stroke(255, 240, 219)
-    strokeWeight(3)
-    fill(dotColor)
-    for (let i = 0; i < this.numRows; i++) {
-      let numDotsInRow = i * 2 + 1
-      const widthOfAllDotsInRow =
-        numDotsInRow * (this.dotSize + this.padding) - this.padding
-      const xCoordinateOfFirstDotInRow =
-        x - widthOfAllDotsInRow / 2 + this.padding
-      for (let j = 0; j < numDotsInRow; j++) {
-        const dotX =
-          xCoordinateOfFirstDotInRow + j * (this.dotSize + this.padding)
-        const dotY = y + i * (this.dotSize + this.padding) + this.padding * 2
-        noStroke()
-        ellipse(dotX, dotY, this.dotSize)
-      }
-    }
-    pop()
-  }
-
-  diamond(x, y, diamondColor) {
-    stroke(255, 240, 219)
-    strokeWeight(3)
-    this.triangleOfDots(x, y - this.cellSize, "up", diamondColor)
-    this.triangleOfDots(x, y + this.cellSize, "down", diamondColor)
-    noStroke()
-  }
-
-  hourglass(c) {
-    fill(c)
-    stroke(255, 240, 219)
-    strokeWeight(3)
-    triangle(
-      this.x,
-      this.y,
-      this.x - this.cellSize,
-      this.y + this.cellSize,
-      this.x + this.cellSize,
-      this.y + this.cellSize
-    )
-    triangle(
-      this.x,
-      this.y,
-      this.x - this.cellSize,
-      this.y - this.cellSize,
-      this.x + this.cellSize,
-      this.y - this.cellSize
-    )
-    noStroke()
-  }
-
-  hourglassWithDiamondToTheRight(hourglassColor) {
-    const diamondColor = random(this.colors.filter((c) => c !== hourglassColor))
-    this.diamond(this.x + this.cellSize, this.y, diamondColor)
-  }
-
-  hourglassWithDiamondToTheLeft(hourglassColor) {
-    const diamondColor = random(this.colors.filter((c) => c !== hourglassColor))
-    this.diamond(this.x - this.cellSize, this.y, diamondColor)
-  }
-
-  hourglassWithTwoTrianglesToTheLeft(hourglassColor) {
+  hourglassWithTwoTrianglesOnLeft(hourglassColor) {
     this.hourglass(hourglassColor)
     const c1 = random(this.colors.filter((c) => c !== hourglassColor))
-    this.triangleOf3Dots(this.x, this.y, "right", c1)
+    this.triangleOfThreeDots(this.x, this.y, "right", c1)
     const c2 = random(
       this.colors.filter((c) => c !== hourglassColor && c !== c1)
     )
     fill(c2)
-    stroke(255, 240, 219)
-    strokeWeight(3)
     triangle(
       this.x - this.cellSize * 2,
       this.y,
@@ -160,16 +293,14 @@ class Shape {
     )
   }
 
-  hourglassWithTwoTrianglesToTheRight(hourglassColor) {
+  hourglassWithTwoTriangleOnRight(hourglassColor) {
     this.hourglass(hourglassColor)
     const c1 = random(this.colors.filter((c) => c !== hourglassColor))
-    this.triangleOf3Dots(this.x, this.y, "left", c1)
+    this.triangleOfThreeDots(this.x, this.y, "left", c1)
     const c2 = random(
       this.colors.filter((c) => c !== hourglassColor && c !== c1)
     )
     fill(c2)
-    stroke(255, 240, 219)
-    strokeWeight(3)
     triangle(
       this.x + this.cellSize * 2,
       this.y,
@@ -180,9 +311,9 @@ class Shape {
     )
   }
 
-  hourglassWithTriangleOnTheLeftAndDiamondOnTheRight(hourglassColor) {
+  hourglassWithTriangleOnLeftAndDiamondOnRight(hourglassColor) {
     const c1 = random(this.colors.filter((c) => c !== hourglassColor))
-    this.triangleOf3Dots(this.x, this.y, "right", c1)
+    this.triangleOfThreeDots(this.x, this.y, "right", c1)
     this.hourglass(hourglassColor)
     const c2 = random(
       this.colors.filter((c) => c !== c1 && c !== hourglassColor)
@@ -190,13 +321,13 @@ class Shape {
     this.diamond(this.x + this.cellSize, this.y, c2)
   }
 
-  hourglassWithTwoTrianglesToTheLeftAndDiamondOnTheRight(hourglassColor) {
-    this.hourglassWithTwoTrianglesToTheLeft(hourglassColor)
-    this.hourglassWithDiamondToTheRight(hourglassColor)
+  hourglassWithTwoTrianglesOnLeftAndDiamondOnRight(hourglassColor) {
+    this.hourglassWithTwoTrianglesOnLeft(hourglassColor)
+    this.hourglassWithDiamondOnRight(hourglassColor)
   }
 
-  hourglassWithTwoTrianglesToTheRightAndDiamondOnTheLeft(hourglassColor) {
-    this.hourglassWithTwoTrianglesToTheRight(hourglassColor)
+  hourglassWithTwoTriangleOnRightAndDiamondOnLeft(hourglassColor) {
+    this.hourglassWithTwoTriangleOnRight(hourglassColor)
     this.hourglassWithDiamondToTheLeft(hourglassColor)
   }
 
@@ -221,119 +352,6 @@ class Shape {
     this.hourglass(hourglassColor)
     // since hourglass is always solid, triangle should be dotted
     const c = random(this.colors.filter((c) => c !== hourglassColor))
-    this.triangleOf3Dots(this.x, this.y, "right", c)
-  }
-
-  draw() {
-    const dotted = random() < 0.5
-    const r = random()
-    const hourglassColor = random(this.colors)
-    if (this.numElements === 4) {
-      if (r < 0.5) {
-        return this.hourglassWithTwoTrianglesToTheLeftAndDiamondOnTheRight(
-          hourglassColor
-        )
-      }
-      return this.hourglassWithTwoTrianglesToTheRightAndDiamondOnTheLeft(
-        hourglassColor
-      )
-    }
-
-    if (this.numElements === 3) {
-      const hourglassColor = random(this.colors)
-      this.hourglass(hourglassColor)
-      if (r < 0.33) {
-        return this.hourglassWithTwoTrianglesToTheLeft(hourglassColor)
-      }
-      if (r < 0.66) {
-        return this.hourglassWithTwoTrianglesToTheRight(hourglassColor)
-      }
-      return this.hourglassWithTriangleOnTheLeftAndDiamondOnTheRight(
-        hourglassColor
-      )
-    }
-    if (this.numElements === 2) {
-      if (r < 0.33) {
-        return this.hourglassWithDiamondToTheRight()
-      }
-      if (r < 0.66) {
-        return this.diamondWithTriangle()
-      }
-      return this.hourglassWithTriangleToTheLeft()
-    }
-    const c = random(this.colors)
-    switch (this.type) {
-      case "left":
-        if (dotted) {
-          this.triangleOfDots(this.x, this.y, "left", c)
-        } else {
-          fill(random(this.colors))
-          triangle(
-            this.x,
-            this.y,
-            this.x + this.cellSize,
-            this.y - this.cellSize,
-            this.x + this.cellSize,
-            this.y + this.cellSize
-          )
-        }
-        break
-      case "right":
-        if (dotted) {
-          this.triangleOfDots(this.x, this.y, "right", c)
-        } else {
-          fill(random(this.colors))
-          triangle(
-            this.x,
-            this.y,
-            this.x - this.cellSize,
-            this.y - this.cellSize,
-            this.x - this.cellSize,
-            this.y + this.cellSize
-          )
-        }
-        break
-      case "up":
-        if (dotted) {
-          this.triangleOfDots(this.x, this.y, "up", c)
-        } else {
-          fill(random(this.colors))
-          triangle(
-            this.x,
-            this.y,
-            this.x - this.cellSize,
-            this.y + this.cellSize,
-            this.x + this.cellSize,
-            this.y + this.cellSize
-          )
-        }
-        break
-      case "down":
-        if (dotted) {
-          this.triangleOfDots(this.x, this.y, "down", c)
-        } else {
-          fill(random(this.colors))
-
-          triangle(
-            this.x,
-            this.y,
-            this.x - this.cellSize,
-            this.y - this.cellSize,
-            this.x + this.cellSize,
-            this.y - this.cellSize
-          )
-        }
-        break
-
-      // diamonds are always dotted
-      case "diamond":
-        this.diamond(this.x, this.y, c)
-        break
-
-      // hourglass is never dotted
-      case "hourglass":
-        this.hourglass(random(this.colors))
-        break
-    }
+    this.triangleOfThreeDots(this.x, this.y, "right", c)
   }
 }
